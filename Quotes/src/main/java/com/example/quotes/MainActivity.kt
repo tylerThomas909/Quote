09d1d0ed1d2d60c5eae0f24ad888fcf39c373704
Tyler <n01344909@humbermail.ca>
2022-05-04 14:01:39 -0400

    initial commit + updated gitignore

---
+++ Quotes/src/main/java/com/example/quotes/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.quotes
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        quoteCall()
+    }
+    fun quoteCall() {
+        val textView: TextView = findViewById(R.id.quoteQuery)
+        Thread {
+            val client = OkHttpClient()
+            val url = URL("https://reqres.in/api/users?page=2")
+            val request = Request.Builder()
+                .url(url)
+                .get()
+                .build()
+            val response = client.newCall(request).execute()
+            val responseBody = response.body!!.string()
+            runOnUiThread {
+                if (response.isSuccessful){
+                    ("Response Body: $responseBody").also { textView.text = it }
+                }else{
+                    "Error".also { textView.text = it }
+                }
+            }
+        }.start()
+    }
+}
